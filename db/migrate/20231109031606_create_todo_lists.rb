319b007497295cff099a9e9ec1fa41f8bd89f874
thallyslemos <thallysvilemos@gmail.com>
2023-11-09 00:32:07 -0300

    remove createdAt

--- db/migrate/20231109031606_create_todo_lists.rb
+++ db/migrate/20231109031606_create_todo_lists.rb
@@ -2,7 +2,6 @@ class CreateTodoLists < ActiveRecord::Migration[7.0]
   def change
     create_table :todo_lists do |t|
       t.string :name
-      t.datetime :created_at
 
       t.timestamps
     end

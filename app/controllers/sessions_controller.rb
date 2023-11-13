edac14c8058e01d369ee398fa21e372f6f6ff14c
thallyslemos <thallysvilemos@gmail.com>
2023-11-13 00:09:50 -0300

    add user data response

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       token = JWT.encode({ user_id: user.id }, 'secretKey@123')
-      render json: { token: token }
+      render json: { token: token, user: { id: user.id, name: user.name, email: user.email }}
     else
       render json: { error: 'Email ou senha inválidos!' }, status: :unauthorized
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_010034) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_013504) do
   create_table "todo_lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_010034) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "login"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
